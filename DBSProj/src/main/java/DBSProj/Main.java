9b4a80a339eb7aa76d14ba03120a041469d6754c
BlackStar09 <73814092+BlackStar09@users.noreply.github.com>
2021-04-11 18:14:15 +0530

    Basic Layout

---
+++ DBSProj/src/main/java/DBSProj/Main.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DBSProj;
+
+/**
+ *
+ * @author Aswath Vinayak K
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Homescreen h =  new Homescreen();
+        h.setVisible(true);
+        Students s =  new Students();
+        s.setVisible(true);
+        Teachers t =  new Teachers();
+        t.setVisible(true);
+        Admin a =  new Admin();
+        a.setVisible(true);
+    }
+    
+}
